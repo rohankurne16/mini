27bad9ca36adf87865c145e6e730e7051404e56a
rohankurne16 <rohankurne16@gmail.com>
2026-06-24 12:39:08 +0530

    updated upload

--- index.php
+++ index.php
@@ -97,10 +97,7 @@
         
     if(isset($_POST['name'])){
 
-	echo "<pre>";
-print_r($_POST);
-echo "</pre>";
-
+	
         $conn = new mysqli($servername, $username, $password, $db);
 
         if($conn->connect_error){
@@ -108,24 +105,7 @@ echo "</pre>";
         }
 		if(isset($_POST['name'])){
 
-    echo "STEP 1<br>";
-
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
-
-    echo "STEP 2<br>";
-
-    $conn = new mysqli($servername, $username, $password, $db);
-
-    echo "STEP 3<br>";
-
-    if($conn->connect_error){
-        die("Connection Failed: " . $conn->connect_error);
-    }
-
-    echo "STEP 4 - Connected<br>";
-}
+   }
 	
 	$name = $_POST["name"];    
         $email = $_POST["email"];
